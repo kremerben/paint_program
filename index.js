2410898cf5b2e54bace8128d3d3941ce7bef05ab
kremerdesign <kremerdesign@gmail.com>
2014-08-14 23:56:17 -0700

    create an image button working

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 $(document).ready(function() {
 
+var width = 1;
+$(function() {
+    $( "#slider" ).slider({
+      value: width,
+      min:1,
+      max:50,
+      orientation: "horizontal",
+      range: "min",
+      animate: true,
+      slide: function( event, ui ) {
+        width = $( "#slider" ).slider( "value" );
+		console.log("w"+width);
+      }
+    });
+  });
+
+
+
 var strokeColor = "black";
 $('.colorbox').on('click', function() {
 	strokeColor = $(this).data("bcolor");
@@ -18,6 +36,11 @@ function findPos(obj) {
     return undefined;
 }
 
+function getWidthSlider() {
+	
+	width = $( "#slider" ).slider( "option", "value" );
+	return width;
+}
 
 var c=document.getElementById("DrawCanvas");
 var ctx=c.getContext("2d");
@@ -31,6 +54,8 @@ ctx.lineWidth=3;
     
 
 $("#DrawCanvas").on("mousemove", function(e) {
+	ctx.lineWidth=getWidthSlider();
+	ctx.lineCap="round";
 
     var pos = findPos(this);
     var x = e.pageX - pos.x;
@@ -53,18 +78,74 @@ $("#DrawCanvas").on("mousemove", function(e) {
 		yStart = yEnd;
     };
 	ctx.stroke();
+	ctx.save();
 
 });
 
-$(document).on("mouseup", function(){
+
+$('#undo').on('click', function() {
+	     ctx.restore();
+});
+
+
+
+$(c).on("mouseup", function(){
 	startNewLine = true;
 });
 
-$(document).on("mousedown", function(){
+$(c).on("mousedown", function(){
 	startNewLine = false;
 });
 
+$(document).on('click', '#saveImage', function(c) {
+/* var c = document.getElementById("sketch"); */
+var dataString = c.toDataURL();
+      document.getElementById('canvasImg').src = dataString;
 
+/* window.open(dataString); */
+});
+
+
+function to_image(){
+    document.getElementById("theimage").src = c.toDataURL();
+}
+
+function download_image(){
+    //Canvas2Image.saveAsPNG(canvas);
+    var image = c.toDataURL("image/png").replace("image/png", "image/octet-stream"); // here is the most important part because if you dont replace you will get a DOM 18 exception.
+/*     window.location.href=image; */
+    
+    
+    $data = base64_decode(image);
+
+list($type, $data) = explode(';', $data);
+list("", $data)      = explode(',', $data);
+$data = base64_decode($data);
+
+/* file_put_contents('image.png', $data); */
+
+var image = new Image();
+image.src = $data;
+document.body.appendChild(image);
+
+
+}
+
+
+
+
+/*
+function save_image_local(){
+	var image = c.toDataURL("image/png").replace("image/png", "image/octet-stream");
+	$.ajax()
+	
+}
+*/
+
+
+document.getElementById('bt_draw').onclick = to_image
+document.getElementById('bt_download').onclick = download_image
+/* document.getElementById('bt_saveLocal').onclick = save_image_local */
 
 
 });
--- paint.js
+++ paint.js
@@ -78,8 +78,16 @@ $("#DrawCanvas").on("mousemove", function(e) {
 		yStart = yEnd;
     };
 	ctx.stroke();
+	ctx.save();
 
 });
+
+
+$('#undo').on('click', function() {
+	     ctx.restore();
+});
+
+
 
 $(c).on("mouseup", function(){
 	startNewLine = true;
@@ -89,7 +97,79 @@ $(c).on("mousedown", function(){
 	startNewLine = false;
 });
 
+$(document).on('click', '#saveImage', function(c) {
+/* var c = document.getElementById("sketch"); */
+var dataString = c.toDataURL();
+      document.getElementById('canvasImg').src = dataString;
 
+/* window.open(dataString); */
+});
+
+
+function to_image(){
+    document.getElementById("theimage").src = c.toDataURL();
+}
+
+function download_image(){
+    //Canvas2Image.saveAsPNG(canvas);
+    var image = c.toDataURL("image/png").replace("image/png", "image/octet-stream"); // here is the most important part because if you dont replace you will get a DOM 18 exception.
+    window.location.href=image;
+    
+    
+    $data = base64_decode(image);
+
+list($type, $data) = explode(';', $data);
+list("", $data)      = explode(',', $data);
+$data = base64_decode($data);
+
+/* file_put_contents('image.png', $data); */
+
+var image = new Image();
+image.src = $data;
+document.body.appendChild(image);
+
+
+}
+
+
+
+
+
+function save_image_local(){
+	var image = c.toDataURL("image/png").replace("image/png", "image/octet-stream");
+/* 	$.ajax() */
+	console.log("1  "+image);
+      // save canvas image as data url (png format by default)
+      var dataURL = c.toDataURL();
+	console.log("2  "+dataURL);
+
+      // set canvasImg image src to dataURL
+      // so it can be saved as an image
+      document.getElementById('canvasImg').src = dataURL;
+
+	
+	var url = 'upload.php';
+data = $('#canvasImg').attr('src');
+	console.log("3  "+data);
+
+$.ajax({ 
+    type: "POST", 
+    url: url,
+    dataType: 'text',
+    data: {
+        base64data : dataURL
+    }
+});
+
+	
+	
+}
+
+
+
+document.getElementById('bt_draw').onclick = to_image
+document.getElementById('bt_download').onclick = download_image
+document.getElementById('bt_saveLocal').onclick = save_image_local
 
 
 });
